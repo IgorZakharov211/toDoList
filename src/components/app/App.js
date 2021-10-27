22c162d6d54b5e27421f09363dfe23ce6c738cd5
Ïgor Zakharov <Igor1994294@yandex.ru>
2021-10-27 13:47:01 +0300

    feat: add app component

---
+++ src/components/app/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Todolist from '../todolist/Todolist';
+
+function App() {
+  return (
+    <div className="app">
+      <Todolist />
+      
+    </div>
+  );
+}
+
+export default App;
